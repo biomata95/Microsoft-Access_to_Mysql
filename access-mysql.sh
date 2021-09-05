b0a0604981a097069489d639923319b9c32f3b12
biomata95 <37086825+biomata95@users.noreply.github.com>
2021-09-05 17:43:14 -0300

    Update access-mysql.sh

--- access-mysql.sh
+++ access-mysql.sh
@@ -1,19 +1,19 @@
-TABLES=$(mdb-tables -1 $1) # A variável recebe a lista de todas as tabelas do banco em Access. Através do comando mdb-tables. No qual o primeiro argumento representa o banco em Access
+TABLES=$(mdb-tables -1 $1) # The variable receives the list of all database tables in Access. Through the mdb-tables command. Where the first argument represents the database in Access
 
-MUSER="root" # Username da conta em mysql
-MPASS="root" # Username da conta em mysql
+MUSER="root" # Username
+MPASS="root" # Password
 MDB="$2"
 
-MYSQL=$(which mysql) # Caminho para o arquivo binario do mysql
+MYSQL=$(which mysql) # Path to mysql binary file
 
-for t in $TABLES # Loop para percorrer as tabelas, incluindo a verificação da existencia de alguma tabela com mesmo nome no banco de destino(Em MYSQL). Se já existe deleta a tabela.
+for t in $TABLES # Loop to go through the tables, including checking the existence of some table with the same name in the destination database (in MYSQL). If it already exists, delete the table.
 do
     $MYSQL -u $MUSER -p$MPASS $MDB -e "DROP TABLE IF EXISTS $t"
 done
 
-mdb-schema $1 mysql | $MYSQL -u $MUSER -p$MPASS $MDB # Extração das características do esquema do banco em access
+mdb-schema $1 mysql | $MYSQL -u $MUSER -p$MPASS $MDB # Extraction of schema characteristics from the in-access bank
 
-for t in $TABLES # Inserção dos dados do access para o banco em Mysql
+for t in $TABLES # Inserting access data to the database in Mysql
 do
     mdb-export -D '%Y-%m-%d %H:%M:%S' -I mysql $1 $t | $MYSQL -u $MUSER -p$MPASS $MDB
 done
